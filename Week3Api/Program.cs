588458d1bcba682e765b998f33d6e34006d0bd57
GilDragon <PAR13371158@student.kangan.edu.au>
2022-10-19 15:20:47 +1100

    forth

--- Week3Api/Program.cs
+++ Week3Api/Program.cs
@@ -5,11 +5,9 @@ var app = builder.Build();
 
 app.MapGet("/add", () => getNum());
 app.MapGet("/add/{lowernumb}/{uppernumb}", (int lowernumb, int uppernumb) => getNum2(lowernumb, uppernumb));
+app.MapGet("/exp/{base1}/{exp}", (int base1, int exp) => getNum3(base1, exp));
 
 
-
-
-    
 int getNum(){
     Random numb = new Random();
     int num = numb.Next(1,100);
@@ -20,5 +18,18 @@ int getNum2(int lowernumb, int uppernumb){
     Random numb = new Random();
     int num = numb.Next(lowernumb, uppernumb);
     return num;
+}
+
+int getNum3(int base1, int exp) {
+
+    var totalnum = 1;
+
+    for 
+        (int i= 0; i < exp; i++) {
+
+    totalnum = totalnum* base1;
+    }
+
+    return totalnum;
 }
 app.Run();
